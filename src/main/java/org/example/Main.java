99809588c24228f7dfd91211b8f21bc0a739d0aa
impROS <impROS@hotmail.com>
2024-01-22 00:01:13 +0300

    sync

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,14 @@ import java.util.concurrent.Executors;
  *  kez kilitleme (locking) ve serbest bırakma (unlocking) işlemi yapabilmesine olanak tanır.
  *  Bu özelliği nedeniyle "reentrant" (yeniden giriş) olarak adlandırılır.
  *
+ * tryLock() metodu, ReentrantLock sınıfının bir metodu olup, bir kilit (lock) almayı denemek için kullanılır.
+ * Bu metot, kilit hemen alınamazsa beklemek yerine anında bir değer döndürür. Eğer kilit alınamıyorsa false,
+ * alınabiliyorsa true değeri döner.
+ *
+ * tryLock() metodunun bir diğer varyantı ise belirli bir süre boyunca kilit almayı denemektir.
+ * Bu durumda, tryLock(long timeout, TimeUnit unit) metodu kullanılır ve belirli bir süre içinde kilit alınamazsa false,
+ *  alınırsa true değeri döner. Bu şekilde, belirli bir süre boyunca beklemenin ardından işlemlere devam edilebilir
+ *  veya başka bir alternatif yol izlenebilir.
  */
 public class Main {
     public static void main(String[] args) {

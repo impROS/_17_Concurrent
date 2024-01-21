de006818a5805305843908ace62088e77c3d91ab
impROS <impROS@hotmail.com>
2024-01-21 03:02:25 +0300

    sync

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -1,2 +1,44 @@
-package org.example;public class BankAccount {
+package org.example;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BankAccount {
+    private final Lock lock = new ReentrantLock();
+    private int balance = 1000;
+
+    public void withdraw(int amount) {
+        lock.lock();
+        try {
+            if (amount > 0 && amount <= balance) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                balance -= amount;
+                System.out.println("Withdrawal successful. Remaining balance: " + balance);
+            } else {
+                System.out.println("Withdrawal failed. Insufficient funds.");
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    //TryLock
+    public void performTask() {
+        if (lock.tryLock()) { // Kilit alınmaya çalışılır
+            try {
+                // Kilitli bölgede yapılacak işlemler
+                System.out.println("Task performed inside the locked region.");
+            } finally {
+                lock.unlock(); // Kilit serbest bırakılır
+            }
+        } else {
+            // Kilit alınamazsa yapılacak işlemler
+            System.out.println("Could not acquire the lock.");
+        }
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+/*
+ * ReentrantLock, Java'nın java.util.concurrent.locks paketinde bulunan bir kilit (lock) implementasyonudur.
+ *  Bu kilit, diğer kilit türlerinden farklı olarak, bir iş parçacığının aynı kilit üzerinde birden fazla
+ *  kez kilitleme (locking) ve serbest bırakma (unlocking) işlemi yapabilmesine olanak tanır.
+ *  Bu özelliği nedeniyle "reentrant" (yeniden giriş) olarak adlandırılır.
+ *
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BankAccount account = new BankAccount();
+
+        Runnable withdrawRunnable1 = () -> {
+            account.withdraw(300);
+        };
+
+        Runnable withdrawRunnable2 = () -> {
+            account.withdraw(500);
+        };
+
+        Runnable withdrawRunnable3 = () -> {
+            account.withdraw(400);
+        };
+
+        // İki iş parçacığını eşzamanlı olarak çalıştırmak için ExecutorService kullanılır.
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(withdrawRunnable1);
+        executorService.submit(withdrawRunnable2);
+        executorService.submit(withdrawRunnable3);
+
+        executorService.shutdown();
     }
 }
